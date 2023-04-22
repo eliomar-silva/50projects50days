aa742e0c6f64f1e4f18c0a2ccf2a7e6bf1d7ab96
Eliomar <eliomarsilva000@gmail.com>
2023-04-21 23:17:03 -0300

    day-42

--- day42/main.js
+++ day42/main.js
@@ -1,18 +1,21 @@
-
+const searchEle = document.getElementById('filter');
 const resultEle = document.getElementById('result');
+const listUser = []
 
+searchEle.addEventListener('input',(e) => searchUser(e.target.value));
 
 const getData = async () => {
   const res = await fetch('https://randomuser.me/api?results=50');
-  const  {results} = await res.json();
+  const  {results} = await res.json(); 
 
-  resultEle.innerHTML = '';
-  
+  resultEle.innerHTML = '';  
 
   results.forEach(user => {
     const { picture, name, location} = user;
 
     const userLi = document.createElement('li');
+
+    listUser.push(userLi);
 
     userLi.innerHTML = `
       <img src="${picture.large}" alt="image of ${name.first} ${name.last}"></img>
@@ -21,9 +24,19 @@ const getData = async () => {
         <p>${location.city}, ${location.country}</p>
       </div>
     `;   
-    
+    userLi.classList.remove
     resultEle.appendChild(userLi);
   })
 }
+
+const searchUser = (search) => {
+  listUser.forEach(item => {
+    if(item.innerText.toLowerCase().includes(search.toLowerCase())) {
+      item.classList.remove('hide');
+    }else {
+      item.classList.add('hide');
+    }
+  })
+} 
 
 getData();
